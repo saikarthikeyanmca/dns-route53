35ad136576330bc1d92559c72edf1ce2d6ff7e49
saikarthikeyanmca <36722876+saikarthikeyanmca@users.noreply.github.com>
2018-03-19 14:08:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,19 @@
 provider "aws" { 
 
-    access_key = "${var.AWS_ACCESS_KEY}"
+    access_key = "${var.aws_access_key}"
 
-    secret_key = "${var.AWS_SECRET_KEY}"
+    secret_key = "${var.aws_secret_key}"
 
-    region = "${var.AWS_REGION}"
+    region = "ap-south-1a"
 
 }
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
